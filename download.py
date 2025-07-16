3d0a2bac2eff39d4d041aed6fbe1d88f79043075
Kilaru Vasudeva <70791259+Vasudeva-bit@users.noreply.github.com>
2025-07-16 19:05:48 +0530

    Create download.py

---
+++ download.py
@@ -0,0 +1,5 @@
+from datasets import load_dataset
+ds = load_dataset("uonlp/CulturaX",
+                  "te",
+                  token="") # use your token on HF, under access tokens; not mine haha.
+ds.save_to_disk("./datasets")
